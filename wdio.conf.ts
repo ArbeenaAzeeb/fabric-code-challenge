5ddb10cfe2951c781c5f6bfa876742a594c4be4a
Arbeena Azeeb <arbeena.azeeb@thoughtworks.com>
2026-02-15 12:14:15 +0530

    Arbeena: Browserstack integration debugging

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -169,6 +169,13 @@ export const config: WebdriverIO.Config = {
         timeout: 180000
     },
 
+    beforeSession: function () {
+      console.log('RUN_ENV:', process.env.RUN_ENV)
+      console.log('BS user exists:', !!process.env.BROWSERSTACK_USERNAME)
+      console.log('BS key exists:', !!process.env.BROWSERSTACK_ACCESS_KEY)
+      console.log('BS app exists:', !!process.env.BS_APP_ID)
+    }
+    
     //
     // =====
     // Hooks
