072430303f5860affc798cac48f7487b630ee3c6
Arbeena Azeeb <arbeena.azeeb@thoughtworks.com>
2026-02-15 14:36:11 +0530

    Arbeena: Split session for bs debug

--- src/config/capabilities/browserstack.capabilities.ts
+++ src/config/capabilities/browserstack.capabilities.ts
@@ -1,32 +1,32 @@
-// export const iosBrowserStack = [{
-//     platformName: 'iOS',
-//     'appium:deviceName': 'iPhone SE 2022',
-//     'appium:platformVersion': '15',
-//     'appium:automationName': 'XCUITest',
-//     'appium:app': process.env.BS_APP_ID,
-  
-//     'bstack:options': {
-//       projectName: 'Fabric Code Challenge',
-//       buildName: `Build-${process.env.GITHUB_RUN_NUMBER || 'local'}`,
-//       sessionName: 'iOS Appium Tests',
-//       debug: true,
-//       networkLogs: true
-//     }
-//   }];
-
-
-export const iosBrowserStack = (orientation: 'PORTRAIT' | 'LANDSCAPE') => [{
+export const iosBrowserStack = [{
     platformName: 'iOS',
     'appium:deviceName': 'iPhone SE 2022',
     'appium:platformVersion': '15',
     'appium:automationName': 'XCUITest',
     'appium:app': process.env.BS_APP_ID,
-    'appium:deviceOrientation': orientation,
+  
     'bstack:options': {
       projectName: 'Fabric Code Challenge',
-      buildName: `Build-${process.env.GITHUB_RUN_NUMBER || 'local'}-${orientation}`,
-      sessionName: `iOS Appium Tests - ${orientation}`,
+      buildName: `Build-${process.env.GITHUB_RUN_NUMBER || 'local'}`,
+      sessionName: 'iOS Appium Tests',
       debug: true,
       networkLogs: true
     }
   }];
+
+
+// export const iosBrowserStack = (orientation: 'PORTRAIT' | 'LANDSCAPE') => [{
+//     platformName: 'iOS',
+//     'appium:deviceName': 'iPhone SE 2022',
+//     'appium:platformVersion': '15',
+//     'appium:automationName': 'XCUITest',
+//     'appium:app': process.env.BS_APP_ID,
+//     'appium:deviceOrientation': orientation,
+//     'bstack:options': {
+//       projectName: 'Fabric Code Challenge',
+//       buildName: `Build-${process.env.GITHUB_RUN_NUMBER || 'local'}`,
+//       sessionName: `iOS Appium Tests - ${orientation}`,
+//       debug: true,
+//       networkLogs: true
+//     }
+//   }];
